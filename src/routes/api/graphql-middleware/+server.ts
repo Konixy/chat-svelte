580b217e7201da71b837bc97b9af8fe1fe8deb36
Konixy <konixy.p@gmail.com>
2025-02-12 19:09:18 +0100

    Debugging

--- src/routes/api/graphql-middleware/+server.ts
+++ src/routes/api/graphql-middleware/+server.ts
@@ -3,20 +3,26 @@ import type { RequestHandler } from './$types';
 import { PUBLIC_GRAPHQL_API, PUBLIC_URL } from '$env/static/public';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const response = await fetch(PUBLIC_GRAPHQL_API, {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Origin: PUBLIC_URL,
-			...request.headers
-		},
-		body: JSON.stringify(await request.json()),
-		credentials: 'include',
-		cache: 'no-store'
-	});
-
-	console.log('cookies:' + request.headers.get('cookie'));
-
-	return json(await response.json());
+	return json(
+		await new Promise(async (resolve, reject) => {
+			fetch(PUBLIC_GRAPHQL_API, {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					Origin: PUBLIC_URL,
+					Cookie: request.headers.get('cookie') || ''
+				},
+				body: JSON.stringify(await request.json()),
+				credentials: 'include',
+				cache: 'no-store'
+			})
+				.then(async (r) => {
+					return resolve(await r.json());
+				})
+				.catch((e) => {
+					return reject(e);
+				});
+		})
+	);
 };
